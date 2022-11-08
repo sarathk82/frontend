b228a88ed2bd527f289e7f4b23fd048143655b60
sarathk82 <84178883+sarathk82@users.noreply.github.com>
2022-11-09 00:39:04 +0530

    warning cleanup

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -14,24 +14,22 @@ function HomeScreen() {
 
     });
 
-});
 
+    return (
+        <div>
+            <h1>
+                Latest Products
+            </h1>
+            <Row>
+                {products.map(product => (
 
-return (
-    <div>
-        <h1>
-            Latest Products
-        </h1>
-        <Row>
-            {products.map(product => (
-
-                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                    <Product product={product} />
-                </Col>
-            ))}
-        </Row>
-    </div>
-);
+                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                        <Product product={product} />
+                    </Col>
+                ))}
+            </Row>
+        </div>
+    );
 }
 
 export default HomeScreen;
